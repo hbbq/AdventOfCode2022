7e2fa147049ddd653ac4102fe8914f05b13ba7f3
Henrik Bergqvist <mail@henrikbergqvist.com>
2022-12-06 06:48:56 +0100

    Day 6

---
+++ src/AdventOfCode2022/Days/Day6.fs
@@ -0,0 +1,28 @@
+﻿module Day6
+
+
+let Problem1 (input : string) = 
+
+    let chars = input |> Common.Chars
+
+    let index =
+        chars
+        |> Seq.windowed 4
+        |> Seq.findIndex (fun e ->
+            (e |> Array.distinct |> Array.length) = 4
+        )
+
+    index + 4
+
+let Problem2 (input : string) = 
+
+    let chars = input |> Common.Chars
+
+    let index =
+        chars
+        |> Seq.windowed 14
+        |> Seq.findIndex (fun e ->
+            (e |> Array.distinct |> Array.length) = 14
+        )
+
+    index + 14
